3461bde95b8f548fba2ff8c120e89b96c9b1392c
frank2 <frank2@dc949.org>
2024-02-09 20:38:21 -0800

    msvc is a curse

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <windows.h>
 #include "headers.h"
 
-extern "C" void __fastcall limbo_hell(uintptr_t callback);
-extern "C" void __fastcall limbos_gate(uint32_t syscall);
-extern "C" limbo_descent();
+extern void __fastcall limbo_hell(uintptr_t callback);
+extern void __fastcall limbos_gate(uint32_t syscall);
+extern limbo_descent();
 
 typedef struct __IATEntry
 {
